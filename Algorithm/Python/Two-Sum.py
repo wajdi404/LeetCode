3286dbd07041e002fd594d4bd4a4f9efbebe030c
Wajdi <62748386+wajdi404@users.noreply.github.com>
2022-07-09 18:33:32 +0100

    Update Two-Sum.py

--- Algorithm/Python/Two-Sum.py
+++ Algorithm/Python/Two-Sum.py
@@ -5,13 +5,23 @@ def twoSum( nums, target):
   :type target: int
   :rtype: List[int]
   """
-  sum = []
-  for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-      if nums[i] + nums[j] == target:
-        sum.append(i)
-        sum.append(j)
-  return sum
+# Solution using brute force
+#   sum = []
+#   for i in range(len(nums)):
+#     for j in range(i+1,len(nums)):
+#       if nums[i] + nums[j] == target:
+#         sum.append(i)
+#         sum.append(j)
+#   return sum
+
+# Solution using Hash table
+  Dict = {}
+  for i, n in enumerate(nums):
+    if (target - n) in Dict:
+      return [ i, Dict[target - n]]
+    else:
+      Dict[n] = i
+  return []
 
 if __name__ == '__main__':
   nums = [2,7,11,15] 
