c876200744399d2f1fe46127e97328e3d4e6a901
Wajdi <62748386+wajdi404@users.noreply.github.com>
2022-07-09 15:41:15 +0100

    Create Two-Sum.py

---
+++ Algorithm/Python/Two-Sum.py
@@ -0,0 +1,22 @@
+
+def twoSum( nums, target):
+  """
+  :type nums: List[int]
+  :type target: int
+  :rtype: List[int]
+  """
+  sum = []
+  for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+      if nums[i] + nums[j] == target:
+        sum.append(i)
+        sum.append(j)
+  return sum
+
+if __name__ == '__main__':
+  nums = [2,7,11,15] 
+  target = 9
+  t0 = time.time()
+  print(twoSum( nums, target)) # output [0,1]
+  t1 = time.time()
+  print(t1 - t0)
